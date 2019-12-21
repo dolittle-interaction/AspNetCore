67b3b1025dc1f27bedae55c87d37fae80ea98b17
Einar Ingebrigtsen <einar@dolittle.com>
2019-12-21 09:51:12 +0100

    Static code analysis fixes

--- Source/Configuration/ExecutionContextSetupOptions.cs
+++ Source/Configuration/ExecutionContextSetupOptions.cs
@@ -1,21 +1,20 @@
-﻿/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+﻿// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace Dolittle.AspNetCore.Configuration
 {
     /// <summary>
-    /// Represents the configuration for the ExecutionContextSetup middleware
+    /// Represents the configuration for the ExecutionContextSetup middleware.
     /// </summary>
     public class ExecutionContextSetupOptions
     {
         /// <summary>
-        /// Gets or sets the name of the HTTP Header that contains the TenantId 
+        /// Gets or sets the name of the HTTP Header that contains the TenantId.
         /// </summary>
         public string TenantIdHeaderName { get; set; } = "Tenant-ID";
+
         /// <summary>
-        /// Gets or sets whether the ExecutionContextSetup middleware should skip authentication
+        /// Gets or sets a value indicating whether the ExecutionContextSetup middleware should skip authentication.
         /// </summary>
         public bool SkipAuthentication { get; set; } = false;
     }
--- Source/Configuration/Options.cs
+++ Source/Configuration/Options.cs
@@ -1,17 +1,15 @@
-﻿/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+﻿// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace Dolittle.AspNetCore.Configuration
 {
     /// <summary>
-    /// Represents the configuration for Dolittle services and middlewares
+    /// Represents the configuration for Dolittle services and middlewares.
     /// </summary>
     public class Options
     {
         /// <summary>
-        /// Gets or sets the configuration of the ExecutionContextSetup middleware <see cref="Dolittle.AspNetCore.Configuration.ExecutionContextSetupOptions"/> 
+        /// Gets the configuration of the ExecutionContextSetup middleware <see cref="ExecutionContextSetupOptions"/>.
         /// </summary>
         public ExecutionContextSetupOptions ExecutionContextSetup { get; } = new ExecutionContextSetupOptions();
     }
