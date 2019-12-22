814b15f03e728e041abfe1cb101b1c33f0140e6c
Einar Ingebrigtsen <einar@dolittle.com>
2019-12-22 09:07:52 +0100

    Static code analysis fixes

--- Source/Commands/CommandRequestBinderProvider.cs
+++ Source/Commands/CommandRequestBinderProvider.cs
@@ -1,7 +1,6 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System;
 using Dolittle.Runtime.Commands;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
@@ -10,16 +9,16 @@ using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 namespace Dolittle.AspNetCore.Commands
 {
     /// <summary>
-    /// Represents a <see cref="IModelBinderProvider"/> for providing <see cref="CommandRequestBinder"/>
+    /// Represents a <see cref="IModelBinderProvider"/> for providing <see cref="CommandRequestBinder"/>.
     /// </summary>
     public class CommandRequestBinderProvider : IModelBinderProvider
     {
         /// <inheritdoc/>
         public IModelBinder GetBinder(ModelBinderProviderContext context)
         {
-            if (context == null)throw new ArgumentNullException(nameof(context));
+            if (context == null) throw new ArgumentNullException(nameof(context));
 
-            if (context.Metadata.ModelType == typeof(CommandRequest))return new BinderTypeModelBinder(typeof(CommandRequestBinder));
+            if (context.Metadata.ModelType == typeof(CommandRequest)) return new BinderTypeModelBinder(typeof(CommandRequestBinder));
 
             return null;
         }
