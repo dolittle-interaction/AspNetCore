c00a74c22eafbdfcc7aaef0012eb0d46f13739a5
Sindre Wilting <sindrewilting@gmail.com>
2020-02-07 09:26:45 +0100

    Start on updating for new runtime

--- Source/Debugging.Commands/CommandsController.cs
+++ Source/Debugging.Commands/CommandsController.cs
@@ -3,7 +3,6 @@
 
 using Dolittle.Artifacts;
 using Dolittle.Commands;
-using Dolittle.PropertyBags;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dolittle.AspNetCore.Debugging.Commands
@@ -15,22 +14,18 @@ namespace Dolittle.AspNetCore.Debugging.Commands
     public class CommandsController : ControllerBase
     {
         readonly IArtifactTypeMap _artifactTypeMap;
-        readonly IObjectFactory _objectFactory;
         readonly ICommandCoordinator _coordinator;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="CommandsController"/> class.
         /// </summary>
         /// <param name="artifactTypeMap">A <see cref="IArtifactTypeMap"/> for mappnig between artifacts and types.</param>
-        /// <param name="objectFactory">A <see cref="IObjectFactory"/> for creating instances of types.</param>
         /// <param name="coordinator">A <see cref="ICommandCoordinator"/> for coordinating commands.</param>
         public CommandsController(
             IArtifactTypeMap artifactTypeMap,
-            IObjectFactory objectFactory,
             ICommandCoordinator coordinator)
         {
             _artifactTypeMap = artifactTypeMap;
-            _objectFactory = objectFactory;
             _coordinator = coordinator;
         }
 
@@ -43,8 +38,8 @@ namespace Dolittle.AspNetCore.Debugging.Commands
         public IActionResult Handle([FromBody] HandleCommandRequest request)
         {
             var type = _artifactTypeMap.GetTypeFor(request.Artifact);
-            var command = _objectFactory.Build(type, request.Command) as ICommand;
-            var result = _coordinator.Handle(request.Tenant, command);
+            // var command = _objectFactory.Build(type, request.Command) as ICommand;
+            var result = _coordinator.Handle(request.Tenant, null);
             return Ok(result);
         }
     }
