9b546e93f51ad622d049427d05c68db0611f6efb
Einar Ingebrigtsen <einar@dolittle.com>
2020-03-02 20:49:48 +0100

    Started work on a sample of the Debugging caps

---
+++ Samples/Debugging/Program.cs
@@ -0,0 +1,39 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Debugging
+{
+    /// <summary>
+    /// The entrypoint.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Main method.
+        /// </summary>
+        /// <param name="args">Arguments for the process.</param>
+        public static void Main(string[] args) =>
+            CreateHostBuilder(args)
+                .Build()
+                .Run();
+
+        /// <summary>
+        /// Create a host builder.
+        /// </summary>
+        /// <param name="args">Arguments for the process.</param>
+        /// <returns>Host builder to build and run.</returns>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .UseEnvironment("Development")
+                        .UseStartup<Startup>();
+                });
+    }
+}
---
+++ Samples/Debugging/Startup.cs
@@ -0,0 +1,73 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Autofac;
+using Dolittle.Booting;
+using Dolittle.DependencyInversion.Autofac;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Debugging
+{
+    /// <summary>
+    /// The startup for Asp.Net Core.
+    /// </summary>
+    public class Startup : IDisposable
+    {
+        ILoggerFactory _loggerFactory;
+
+        BootloaderResult _bootResult;
+
+        /// <summary>
+        /// Configure all services.
+        /// </summary>
+        /// <param name="services"><see cref="IServiceCollection"/> to configure.</param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            _loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddConsole();
+                builder.AddEventSourceLogger();
+            });
+
+            _bootResult = services.AddDolittle(_loggerFactory);
+        }
+
+        /// <summary>
+        /// Configure the Autofac container.
+        /// </summary>
+        /// <param name="containerBuilder"><see cref="ContainerBuilder"/> to configure.</param>
+        public void ConfigureContainer(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.AddDolittle(_bootResult.Assemblies, _bootResult.Bindings);
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"><see cref="IApplicationBuilder"/>.</param>
+        /// <param name="env"><see cref="IWebHostEnvironment"/>.</param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => endpoints.MapGet("/", async context => await context.Response.WriteAsync("Hello World!").ConfigureAwait(false)));
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            _loggerFactory.Dispose();
+        }
+    }
+}
