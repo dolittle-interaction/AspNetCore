6a5fc2b29ce774ee883479b1483cf57fb787f1fb
Joel Hoisko <joel.hoisko@gmail.com>
2020-04-07 12:30:29 +0200

    Add unused EventHandler for Schemas

--- Source/Debugging/Generators/Schemas/EventHandler.cs
+++ Source/Debugging/Generators/Schemas/EventHandler.cs
@@ -2,24 +2,24 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using Dolittle.Concepts;
+using Dolittle.Events;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Dolittle.AspNetCore.Generators.Schemas
 {
     /// <summary>
-    /// An implementation of <see cref="SchemaGeneratorHandler"/> that generates schemas for <see cref="ConceptAs{T}"/> types.
+    /// An implementation of <see cref="SchemaGeneratorHandler"/> that generates schemas for <see cref="IEvent"/> types.
     /// </summary>
-    public class ConceptHandler : SchemaGeneratorHandler
+    public class EventHandler : SchemaGeneratorHandler
     {
         readonly SchemaGenerator _schemaGenerator;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="ConceptHandler"/> class.
+        /// Initializes a new instance of the <see cref="EventHandler"/> class.
         /// </summary>
         /// <param name="schemaGenerator">The <see cref="SchemaGenerator"/> that calls this handler.</param>
-        public ConceptHandler(SchemaGenerator schemaGenerator)
+        public EventHandler(SchemaGenerator schemaGenerator)
         {
             _schemaGenerator = schemaGenerator;
         }
@@ -28,13 +28,13 @@ namespace Dolittle.AspNetCore.Generators.Schemas
         public override bool CanCreateSchemaFor(Type type, out bool shouldBeReferenced)
         {
             shouldBeReferenced = false;
-            return type.IsConcept();
+            return typeof(IEvent).IsAssignableFrom(type);
         }
 
         /// <inheritdoc/>
         public override OpenApiSchema CreateSchema(Type type, SchemaRepository schemaRepository)
         {
-            return _schemaGenerator.GenerateSchema(type.GetConceptValueType(), schemaRepository);
+            return _schemaGenerator.GenerateSchema(type, schemaRepository);
         }
     }
 }
