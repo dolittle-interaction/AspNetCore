a4120fb2e66667c289736e503f0d4cd67233f8fb
Joel Hoisko <joel.hoisko@gmail.com>
2020-04-14 10:38:14 +0200

    Add IMongocollection to the query. Not working rn

--- Samples/Debugging/MyFeature/MyQuery.cs
+++ Samples/Debugging/MyFeature/MyQuery.cs
@@ -1,9 +1,9 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
 using System.Linq;
 using Dolittle.Queries;
+using MongoDB.Driver;
 
 namespace Debugging.MyFeature
 {
@@ -13,8 +13,14 @@ namespace Debugging.MyFeature
     public class MyQuery : IQueryFor<MyReadModel>
     {
         /// <summary>
+        /// Initializes a new instance of the <see cref="MyQuery"/> class.
+        /// </summary>
+        /// <param name="collection">Collection of all the MyReadModel's.</param>
+        public MyQuery(IMongoCollection<MyReadModel> collection) => Query = collection?.AsQueryable();
+
+        /// <summary>
         /// Gets an empty Array.
         /// </summary>
-        public IQueryable<MyReadModel> Query => Array.Empty<MyReadModel>().AsQueryable();
+        public IQueryable<MyReadModel> Query { get; }
     }
 }
